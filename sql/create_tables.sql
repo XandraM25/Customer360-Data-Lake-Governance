49fd2fe41ada20d6b31b6b7edaa434c3a3dc9e04
XandraM25 <95399275+XandraM25@users.noreply.github.com>
2026-05-31 11:26:31 -0400

    Added fact_account_behavior table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -9,3 +9,14 @@ CREATE OR REPLACE TABLE dim_customer (
     customer_status STRING,
     created_at TIMESTAMP
 );
+
+CREATE OR REPLACE TABLE fact_account_behavior (
+    behavior_id BIGINT,
+    customer_id BIGINT,
+    credit_score INTEGER,
+    balance DECIMAL(18,2),
+    num_of_products INTEGER,
+    has_credit_card BOOLEAN,
+    is_active_member BOOLEAN,
+    record_date DATE
+);
